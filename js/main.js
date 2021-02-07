53c38e378867582de6e713d9c53a349ca2ae6250
Roo-Git <robert.martinez.delafuente@gmail.com>
2021-02-07 18:06:27 +0100

    Tarea: onclick fase0.

--- js/main.js
+++ js/main.js
@@ -6,9 +6,10 @@ new Promise (res => setTimeout(() => res(delay), delay));
 
 /* Funcion: Tiempo de Delay Entre pantalla 0 y 1 */ /* FUNCIONA */
 
-resolveIn(1000).then(delay => {
-    cambiarPantalla("fase0","fase1");   /* ID DIV MAESTRO DE CADA PANTALLA */
-})
+/* resolveIn(9000).then(delay => {
+    cambiarPantalla("fase0","fase1");   
+
+}) */
 
 
 /* Clases */
